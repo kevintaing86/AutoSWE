9916787b8aab277277a87163be35198e05f3ea99
Kevin Taing <kevintaing86@gmail.com>
2016-09-12 15:39:00 -0500

    -Created a controller for the main screen -Added 2 buttons and a text field to the view -Added an imageView to the view

---
+++ AutoSWE/AutoSWE/MainScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainScreenViewController.swift
+//  AutoSWE
+//
+//  Created by Kevin Taing on 9/12/16.
+//  Copyright © 2016 Kevin Taing. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
