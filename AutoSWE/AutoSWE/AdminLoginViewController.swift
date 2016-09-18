b9a976a1c019a7ad28fb619652571f680b992d32
Kevin Taing <kevintaing86@gmail.com>
2016-09-18 13:32:10 -0500

    -Redesigned the Admin Login view -Connected the view and its components with the controller

--- AutoSWE/AutoSWE/AdminLoginViewController.swift
+++ AutoSWE/AutoSWE/AdminLoginViewController.swift
@@ -11,15 +11,33 @@ import UIKit
 class AdminLoginViewController: UIViewController {
 
     // MARK: - Outlets and members
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     // MARK: - Actions and methods
     @IBAction func cancel(_ sender: AnyObject) {
         performSegue(withIdentifier: "unwindToMainScreen", sender: nil)
     }
     
+    @IBAction func login(_ sender: AnyObject) {
+        if(usernameTextField.text == ""){
+            errorLabel.text = "must enter a username"
+            errorLabel.isHidden = false
+        }
+    }
+    
+    @IBAction func forgotPassword(_ sender: AnyObject) {
+    }
+    
+    @IBAction func signUp(_ sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        errorLabel.isHidden = true
+        scrollView.contentSize.height = 400
+        self.dismissKeyboard()
     }
 }
