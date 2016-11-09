76788f99c3af6ba11f36641ff4ba7accd5718f01
KevinTaing86 <kevintaing86@gmail.com>
2016-11-08 23:46:05 -0600

    Started to make the select survey view. Added the images to Asset Catalog

---
+++ AutoSWE/AutoSWE/SurveyModel.swift
@@ -0,0 +1,12 @@
+//
+//  SurveyModel.swift
+//  AutoSWE
+//
+//  Created by Kevin Taing on 11/8/16.
+//  Copyright © 2016 Kevin Taing. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var Survey_Templates: [UIImage] = [UIImage(named: "Student_Survey")!, UIImage(named: "Adult_Survey")!, UIImage(named: "Volunteer_Survey")!]
